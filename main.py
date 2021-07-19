7b12f59bc25a03ab631ce355c9be1c2684fec166
chpdrx <thewinniex@gmail.com>
2021-07-19 21:51:48 +0300

    in progress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
+from GoogleParser import Parser
+import random
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'width', '550')
 Config.set('graphics', 'height', '400')
@@ -10,8 +12,20 @@ from kivy.core.window import Window
 Window.clearcolor = (.92, .92, .96, 1)
 
 class MyCaptcha(GridLayout):
-    def press_button_attempt(self):
+    def random_link(self):
+        img_links = Parser('Котик', 'Кефир')
+        imgs = img_links.find_false_links() + [img_links.find_true_links()]
+        self.true_link = img_links.find_true_links()
+        link = imgs[random.randint(0,3)]
+        imgs.remove(link)
+        return link
+
+    def click_holder(self):
         pass
+
+    def press_button_attempt(self):
+        if self.image1.source == self.true_link:
+            return print('123')
 
     def press_button_reset(self):
         pass
